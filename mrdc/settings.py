ddb5192cf91fdd7c2cf844a02d90c3961cb57e44
HShehu <harandemshehu@gmail.com>
2019-12-17 20:06:42 +0300

    Not much

--- mrdc/settings.py
+++ mrdc/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '_yhqy+-8j-z$o07$*k9o#9bzfcvd%5efkyo*f=&shtjxiwcprk'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['harande.pythonanywhere.com']
+ALLOWED_HOSTS = ['harande.pythonanywhere.com','127.0.0.1']
 
 
 # Application definition
