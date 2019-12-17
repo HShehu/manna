1c28872060798179425809a3adf920471fccad28
HShehu <harandemshehu@gmail.com>
2019-12-17 20:54:14 +0300

    Fix

--- mrdc/urls.py
+++ mrdc/urls.py
@@ -21,7 +21,7 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('manna.urls')),
-    # path('blog/', include('blog.urls')),
+    path('blog/', include('blog.urls')),
 ] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
 
 admin.site.site_header = "MRDC Admin"
